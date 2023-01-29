28bed009edf9b102ef2cc37b026fc79a3d297679
Stach13 <112652730+Stach13@users.noreply.github.com>
2023-01-29 15:45:42 -0500

    python-nlp added to a functioning and not stupid directory

---
+++ python/nlp-ex1.py
@@ -0,0 +1,16 @@
+with open("ChatGPT.txt") as file:
+    data = file.read
+
+print(data)
+
+#here I added a find()
+txt = "Here from the king's mountain view"
+x = txt.find("king's mountain")
+print(x)
+
+Tool = ["Jambi", "10,000 days",]
+The_Mountain_Goats = ["King's Mountain", "Goths",]
+for x in Tool:
+    x == Tool
+    x != The_Mountain_Goats
+print(x)
